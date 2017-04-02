38cd5b3c35c59c2a0eaf566a2c4478be56a24745
Brendan Moran <b.daniel.moran@gmail.com>
2017-04-02 18:42:17 -0400

    Set up basic request.

--- app.js
+++ app.js
@@ -1,4 +1,15 @@
+const https = require('https');
+const username = "brendanmoran";
+
+function printMessage (username, badgeCount, points) {
+    const message = (`${username} has ${badgeCount} total badges and ${points} total points in javaScript.`);
+    console.log(message);
+}
+
 //connect to api url
+const request = https.get(`https://teamtreehouse.com/${username}.json`, response => {
+    console.dir(response);
+});
 //read the data
 //parse the data
 //print the data
