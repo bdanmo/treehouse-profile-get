d2431dda683032e99ff96614945d8de17483d9be
Brendan Moran <b.daniel.moran@gmail.com>
2017-04-02 20:56:08 -0400

    Set up command line argument capturing.

--- app.js
+++ app.js
@@ -1,25 +1,28 @@
 
 
 const https = require('https');
-const username = "brendanmoran";
+const users = process.argv.slice(2);
 
 function printMessage (username, badgeCount, points) {
     const message = (`${username} has ${badgeCount} total badges and ${points} points in javaScript.`);
     console.log(message);
 }
 
-//connect to api url
-const request = https.get(`https://teamtreehouse.com/${username}.json`, response => {
-    let body = "";
-    //read the data
-    response.on('data', data => {
-        body += data.toString();
+function getProfile(username) {
+    //connect to api url
+    const request = https.get(`https://teamtreehouse.com/${username}.json`, response => {
+        let body = "";
+        //read the data
+        response.on('data', data => {
+            body += data.toString();
+        });
+        response.on('end', () => {
+            //parse the data
+            const profile = JSON.parse(body);
+            //print the data
+            printMessage(username, profile.badges.length, profile.points.JavaScript);
+        });
     });
-    response.on('end', () => {
-        //parse the data
-        const profile = JSON.parse(body);
-        //print the data
-        printMessage(username, profile.badges.length, profile.points.JavaScript);
-    });
-});
+}
 
+users.forEach(getProfile);
