14c0fa7910cb8ef685ebe308085e4362563ea3f2
Brendan Moran <b.daniel.moran@gmail.com>
2017-04-02 20:44:03 -0400

    Set up parsing and printing of data.

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
+
+
 const https = require('https');
 const username = "brendanmoran";
 
 function printMessage (username, badgeCount, points) {
-    const message = (`${username} has ${badgeCount} total badges and ${points} total points in javaScript.`);
+    const message = (`${username} has ${badgeCount} total badges and ${points} points in javaScript.`);
     console.log(message);
 }
 
@@ -15,7 +17,9 @@ const request = https.get(`https://teamtreehouse.com/${username}.json`, response
     });
     response.on('end', () => {
         //parse the data
+        const profile = JSON.parse(body);
         //print the data
+        printMessage(username, profile.badges.length, profile.points.JavaScript);
     });
 });
 
